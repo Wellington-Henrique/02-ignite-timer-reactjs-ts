0db8bc9aed64c65f91c759fa47831433d056d24b
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-26 20:37:56 -0300

    Páginas e rotas: React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 
-import { Button } from './components/Button'
-
 import { GlobalStyle } from './styles/globals.ts'
+import { Router } from './Router.tsx'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
