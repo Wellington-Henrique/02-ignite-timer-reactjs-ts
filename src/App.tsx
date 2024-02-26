754a980d70de3d3da7ed582ded8208f95f4328e9
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-26 19:41:40 -0300

    Estrutura da Aplicação: Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default';
 
 import { Button } from "./assets/Button";
+import { GlobalStyle } from './styles/globals';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button variant='success'/>
-      <Button/>
+        <GlobalStyle/>
+        <Button variant='primary'/>
+        <Button variant='secondary'/>
+        <Button variant='danger'/>
+        <Button variant='success'/>
+        <Button/>
     </ThemeProvider>
   )
 }
