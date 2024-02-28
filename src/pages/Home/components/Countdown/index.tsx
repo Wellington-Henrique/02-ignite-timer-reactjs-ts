f66f31da5b7b9583cf2f2025c8c215ab094abfd3
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-27 22:56:34 -0300

    Reducers: Salvando estado no storage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -29,7 +29,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondDifference >= totalSeconds) {
